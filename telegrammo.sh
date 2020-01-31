472c3fec5419207bf2bb68d3edf1faaa09977891
Fabio Artuso <me@fabioartuso.com>
2020-01-31 17:43:55 +0000

    telegrammo.sh: better logic

--- telegrammo.sh
+++ telegrammo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
 # If program is not already running, start it 
-HOWMANY="$(ps aux | grep 'node telegrammo.js' | wc -l)" 
-if [ "$HOWMANY" -lt 2 ] 
+HOWMANY="$(ps aux | grep 'node telegrammo.js' | grep -v 'grep' | wc -l)" 
+if [ "$HOWMANY" -lt 1 ] 
 then 
     cd $PWD 
     node telegrammo.js "$@"
